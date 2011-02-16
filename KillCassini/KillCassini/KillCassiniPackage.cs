19a4eb32ded25f441032ee94178160345d1f2800
Latish Sehgal <latish.sehgal@gmail.com>
2011-02-16 17:14:23 -0600

    Added user notification. Removed some auto-generated crap.

--- KillCassini/KillCassini/KillCassiniPackage.cs
+++ KillCassini/KillCassini/KillCassiniPackage.cs
@@ -3,103 +3,71 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.ComponentModel.Design;
-using EnvDTE80;
-using Microsoft.Win32;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 
 namespace LatishSehgal.KillCassini
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the informations needed to show the this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidKillCassiniPkgString)]
     public sealed class KillCassiniPackage : Package
     {
-        /// <summary>
-        /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
-        /// initialization is the Initialize method.
-        /// </summary>
+        private IVsOutputWindowPane _debugPane;
+
         public KillCassiniPackage()
         {
             Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
         }
 
-
-
-        /////////////////////////////////////////////////////////////////////////////
-        // Overriden Package Implementation
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initilaization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
-            Trace.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
             base.Initialize();
 
-            // Add our command handlers for menu (commands must exist in the .vsct file)
-            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if ( null != mcs )
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
             {
-                // Create the command for the menu item.
-                CommandID menuCommandID = new CommandID(GuidList.guidKillCassiniCmdSet, (int)PkgCmdIDList.cmdidKillCassini);
-                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID );
-                mcs.AddCommand( menuItem );
+                var menuCommandID = new CommandID(GuidList.guidKillCassiniCmdSet, (int)PkgCmdIDList.cmdidKillCassini);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
             }
         }
-        #endregion
 
-        /// <summary>
-        /// This function is the callback used to execute a command when the a menu item is clicked.
-        /// See the Initialize method to see how the menu item is associated to this function using
-        /// the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            // Show a Message Box to prove we were here
-            IVsUIShell uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
-            Guid clsid = Guid.Empty;
-            int result;
-            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
-                       0,
-                       ref clsid,
-                       "KillCassini",
-                       string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
-                       string.Empty,
-                       0,
-                       OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                       OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
-                       OLEMSGICON.OLEMSGICON_INFO,
-                       0,        // false
-                       out result));
+            try
+            {
+                var processesKilled = CassiniUtil.KillAllCassiniInstances();
 
-            var dte = (DTE2) GetService(typeof (SDTE));
-            CassiniUtil.KillAllCassiniInstances();
-
+                DebugPane.Activate();
+                DebugPane.OutputString("KillCassini: Mission Acquired. Hunting... \r\n");
+                processesKilled.ForEach(p => DebugPane
+                            .OutputString(String.Format("KillCassini: Killed {0}: Id= {1}, Handle= {2}.\r\n", 
+                            p.Name, p.Id, p.Handle)));
+                DebugPane.OutputString("KillCassini: Over and Out. \r\n");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
         }
 
+        private IVsOutputWindowPane DebugPane
+        {
+            get
+            {
+                if (_debugPane == null)
+                {
+                    var outputWindow = GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+                    var debugPaneGuid = VSConstants.GUID_OutWindowDebugPane;
+                    if (outputWindow != null)
+                        outputWindow.GetPane(ref debugPaneGuid, out _debugPane);
+                }
+                return _debugPane;
+            }
+        }
     }
 }
