33fd10fa46442c04377e82304b6f08742a2b4cda
Matt Anderson <clubanderson@me.com>
2017-05-02 14:25:45 -0500

    Migrated to VS2017.  Switched to NuGet packages.  Changed target framework to 4.6.1

---
+++ KillCassini/Backup1/KillCassini/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace LatishSehgal.KillCassini
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidKillCassini =        0x100;
+
+
+    };
+}
---
+++ KillCassini/Backup1/KillCassini/ProcessInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LatishSehgal.KillCassini
+{
+    internal struct ProcessInfo
+    {
+        public Int32 Id;
+        public IntPtr Handle;
+        public string Name;
+    }
+}
